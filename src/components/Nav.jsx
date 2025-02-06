04a73290b07eab016e0496759084b204068984a5
Katie Nink Smith <katiej.smith@outlook.com>
2025-02-05 23:39:56 -0700

    adjusting nav

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,7 +1,15 @@
 import React from "react";
 
 function Nav() {
-  return <div className="nav">Nav</div>;
+  return (
+    <div className="nav">
+      <img
+        src="./public/logo.png"
+        alt="less then grater then and the letters K N S"
+        width="7%"
+      />
+    </div>
+  );
 }
 
 export default Nav;

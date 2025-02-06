e995e335d4241f8a1f65e9b459e158f678bf34d6
Katie Nink Smith <katiej.smith@outlook.com>
2025-02-05 23:34:27 -0700

    Initial set up with planned components and spacing for layout

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import "./App.css";
+import Nav from "./components/Nav";
+import Main from "./components/Main";
+import About from "./components/About";
+import Projects from "./components/Projects";
+import Contact from "./components/Contact";
+import Footer from "./components/Footer";
+
+function App() {
+  return (
+    <>
+      <Nav />
+      <Main />
+      <About />
+      <Projects />
+      <Contact />
+      <Footer />
+    </>
+  );
+}
+
+export default App;

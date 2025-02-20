07ce27d8a73aa12541699dfba1e5ee4a6fde77fb
Katie Nink Smith <katiej.smith@outlook.com>
2025-02-19 20:56:16 -0700

    projects carousel buttons functioning correctly with information displayed if it is avalible for project

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,26 +1,50 @@
 import { Link } from "react-router";
 import { HiOutlineArrowSmLeft, HiOutlineArrowSmRight } from "react-icons/hi";
+import { useState } from "react";
 
 function Projects() {
-  const projects = {
-    name: "Toddler Words",
-    description: "Toddler language learning application",
-    image: "./welcomePageNoUser.png",
-    deployedURLFront: "https://toddlerwords.netlify.app/",
-    deployedURLBack: "https://toddler-words-backend.onrender.com/",
-    githubFront: "https://github.com/KatieNinkSmith/Toddler_Words_Frontend",
-    githunBack: "https://github.com/KatieNinkSmith/Toddler_Words_Backend",
-    toolsUsed:
-      "Frontend: React.js Backend: Node.js, Express.js Database: MongoDB, Firebase Audio API: Dictionary API Authentication: JWT, bcrypt",
-  };
+  // add additional projects ASAP as the buttons do not work currently
+  const [curProIndex, setCurProIndex] = useState(0);
+
+  const projects = [
+    {
+      name: "Toddler Words",
+      description: "Toddler language learning application",
+      image: "./welcomePageNoUser.png",
+      deployedURLFront: "https://toddlerwords.netlify.app/",
+      deployedURLBack: "https://toddler-words-backend.onrender.com/",
+      githubFront: "https://github.com/KatieNinkSmith/Toddler_Words_Frontend",
+      githunBack: "https://github.com/KatieNinkSmith/Toddler_Words_Backend",
+      toolsUsed:
+        "Frontend: React.js Backend: Node.js, Express.js Database: MongoDB, Firebase Audio API: Dictionary API Authentication: JWT, bcrypt",
+    },
+    {
+      name: "",
+      description: "",
+      image: "",
+      deployedURLFront: "",
+      deployedURLBack: "",
+      githubFront: "",
+      githunBack: "",
+      toolsUsed: "",
+    },
+  ];
+
   const prevProject = () => {
     const prevIndex = (curProIndex - 1 + projects.length) % projects.length;
-    onWordChange(prevIndex);
+    onProChange(prevIndex);
   };
   const nextProject = () => {
     const nextIndex = (curProIndex + 1) % projects.length;
-    onWordChange(nextIndex);
+    onProChange(nextIndex);
   };
+
+  const onProChange = (newIndex) => {
+    setCurProIndex(newIndex);
+  };
+
+  const currentProject = projects[curProIndex];
+
   return (
     <div className="projects">
       <div className="carousel">
@@ -28,22 +52,34 @@ function Projects() {
           <HiOutlineArrowSmLeft />
         </button>
         <div>
-          <h2>{projects.name}</h2>
-          <p>{projects.description}</p>
-          <img src={projects.image} width="150" />
-          <h4>
-            Live link:
-            <Link to={projects.deployedURLFront} target="_blank">
-              Toddler words frontend
-            </Link>
-          </h4>
-          <h4>
-            Live link:
-            <Link to={projects.deployedURLBack} target="_blank">
-              Toddler words backend
-            </Link>
-          </h4>
-          <p>Tools used: {projects.toolsUsed}</p>
+          <h2>{currentProject.name}</h2>
+          <p>{currentProject.description}</p>
+          <img src={currentProject.image} width="150" />
+          {currentProject.deployedURLFront ? (
+            <h4>
+              Live link:
+              <Link to={currentProject.deployedURLFront} target="_blank">
+                {currentProject.name} frontend
+              </Link>
+            </h4>
+          ) : (
+            " "
+          )}
+          {currentProject.deployedURLBack ? (
+            <h4>
+              Live link:
+              <Link to={currentProject.deployedURLBack} target="_blank">
+                {currentProject.name} backend
+              </Link>
+            </h4>
+          ) : (
+            " "
+          )}
+          {currentProject.toolsUsed ? (
+            <p>Tools used: {currentProject.toolsUsed}</p>
+          ) : (
+            " "
+          )}
         </div>
         <button onClick={nextProject}>
           <HiOutlineArrowSmRight />
